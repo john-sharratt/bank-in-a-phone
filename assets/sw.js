9890a3dcf5f0857254f2cecea3dad7d1e945f1ab
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2024-02-29 04:07:04 +1100

    Refactored the app into a banking example

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './bank_in_a_phone.js',
+  './bank_in_a_phone_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
